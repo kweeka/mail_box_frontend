830f6d6c9626cf98a79e9edf0b309573434a6d27
Victoria Bakhturova <kweeka@yandex.ru>
2020-04-12 16:00:42 +0300

    change mobile button more

--- src/components/mailDeletedComponent/mailDeletedComponent.js
+++ src/components/mailDeletedComponent/mailDeletedComponent.js
@@ -12,6 +12,10 @@
         var ctrl = this;
         ctrl.$onInit = function () {
             ctrl.emails = mailStorage.getEmails();
+            ctrl.countMailBox = mailStorage.getCountDeleted();
+            if(mailStorage.getEmails().length < mailStorage.getCountDeleted()){
+                ctrl.showMoreMobile= true;
+            }
         };
     }
 })();
