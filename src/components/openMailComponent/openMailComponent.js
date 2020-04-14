de9e4aca07327089f269602099913c8f64422b9b
Victoria Bakhturova <kweeka@yandex.ru>
2020-04-14 15:20:18 +0300

    fix unread number

--- src/components/openMailComponent/openMailComponent.js
+++ src/components/openMailComponent/openMailComponent.js
@@ -4,7 +4,7 @@
         controller: openMailController
     });
 
-    function openMailController($stateParams, mailService) {
+    function openMailController($stateParams, mailService, mailStorage) {
         var ctrl = this;
         ctrl.mailId = $stateParams.mailId;
         ctrl.$onInit = function () {
@@ -14,6 +14,14 @@
                         console.log(response);
                         ctrl.email = new Email(response.data.response.id, response.data.response.subject, response.data.response.sender,
                             response.data.response.message, response.data.response.read, new Date(response.data.response.date));
+                        mailService.getMailInbox(1, 1)
+                            .then(function success(response) {
+                                mailStorage.setEmails(null, response.data.response.count_inbox, response.data.response.count_outbox, response.data.response.count_deleted,
+                                    response.data.response.count_unread);
+                                return null;
+                            }, function error() {
+                                return null;
+                            });
                     }, function (response) {
                         console.log(response);
                     })
